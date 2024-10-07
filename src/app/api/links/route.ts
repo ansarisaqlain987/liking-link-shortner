f9f65ecfb7c2a05aa0d74f8a211df9783c3f1dcd
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-10-07 14:26:51 +0530

    added routes for the apis

---
+++ src/app/api/links/route.ts
@@ -0,0 +1,4 @@
+export async function GET(){
+    // protected api
+    // return list of links for the user
+}
---
+++ src/app/api/ls/[key]/route.ts
@@ -0,0 +1,4 @@
+export async function GET(){
+    // public API
+    // get the shortened link from 'key' and redirect to the actual url in the new tab
+}
