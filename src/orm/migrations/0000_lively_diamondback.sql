7dc9be5fa7e94c44720d6e874a0bfb719cc8e6c9
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-10-08 16:02:36 +0530

    added tables

--- src/orm/migrations/0000_lively_diamondback.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users_test" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"full_name" text,
-	"phone" varchar(256)
-);
--- src/orm/schema.ts
+++ src/orm/schema.ts
@@ -1,7 +1,18 @@
 import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('users_test', {
+export const users = pgTable('users', {
+  id: varchar('id').primaryKey(),
+  firstName: varchar('first_name', {length: 50}),
+  lastName: varchar('last_name', {length: 50}),
+  email: varchar('email', {length: 100}),
+});
+
+export const links = pgTable('links', {
   id: serial('id').primaryKey(),
-  fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
+  key: varchar('key', {length: 100}).unique(),
+  hash: varchar('hash', {length: 500}).unique(),
+
+  description: text('desc'),
+  name: varchar('name', {length: 250}),
+  link: text('link').notNull(),
 });
