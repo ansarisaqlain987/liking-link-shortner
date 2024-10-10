4297342ca525829cbc5f9527b578bdf11279f7c8
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-10-10 16:43:03 +0530

    added get api

--- src/app/api/links/[id]/route.ts
+++ src/app/api/links/[id]/route.ts
@@ -1,6 +1,27 @@
-export async function GET() {
-  // protected api
-  // return details of the link from id
+import { db } from "@/orm/db";
+import { links } from "@/orm/schema";
+import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { userId } = auth();
+  if (!userId) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const record = await db
+    .select()
+    .from(links)
+    .where(and(eq(links.userId, userId), eq(links.id, Number(params.id))));
+
+  if (record.length > 0) {
+    return Response.json({ link: record[0] });
+  } else {
+    return Response.json({ error: "Not found" }, { status: 404 });
+  }
 }
 
 export async function POST() {
